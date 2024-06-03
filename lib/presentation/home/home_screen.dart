778726b2e9070c3bdca2a87afba845cbe6352402
Tedmyles <mylesteddy7@gmail.com>
2024-06-03 09:58:51 +0300

    ui_modifications

--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -14,11 +14,13 @@ class HotelListScreen extends StatefulWidget {
 class _HotelListScreenState extends State<HotelListScreen> {
   int _currentIndex = 0;
   late Future<List<Hotel>> _hotelsFuture;
+  late Future<List<Destination>> _destinationsFuture;
 
   @override
   void initState() {
     super.initState();
     _hotelsFuture = fetchHotels();
+    _destinationsFuture = fetchDestinations();
   }
 
   Future<List<Hotel>> fetchHotels() async {
@@ -28,27 +30,74 @@ class _HotelListScreenState extends State<HotelListScreen> {
     }).toList();
   }
 
+  Future<List<Destination>> fetchDestinations() async {
+    QuerySnapshot destinationsSnapshot = await FirebaseFirestore.instance.collection('destinations').get();
+    return destinationsSnapshot.docs.map((doc) {
+      return Destination.fromMap(doc.data() as Map<String, dynamic>, doc.id);
+    }).toList();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Hotel List'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.refresh),
-            onPressed: () {
-              setState(() {
-                _hotelsFuture = fetchHotels();
-              });
-            },
-          ),
-        ],
-      ),
-      body: IndexedStack(
-        index: _currentIndex,
+      body: Column(
         children: [
-          buildHotelList(),
-          const ProfilePage(),
+          Container(
+            color: Colors.purple, // Purple color for the top navigation bar
+            padding: const EdgeInsets.only(top: 40, bottom: 10), // Add padding for status bar and bottom space
+            child: const Center(
+              child: Text(
+                'Home',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: const [
+                Text(
+                  'Hotels',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: IndexedStack(
+              index: _currentIndex,
+              children: [
+                ListView(
+                  children: [
+                    buildHotelList(),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: const [
+                          Text(
+                            'Destinations',
+                            style: TextStyle(
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    buildDestinationList(),
+                  ],
+                ),
+                const ProfilePage(),
+              ],
+            ),
+          ),
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(
@@ -84,71 +133,121 @@ class _HotelListScreenState extends State<HotelListScreen> {
           return const Center(child: Text('No hotels found'));
         } else {
           final hotels = snapshot.data!;
-          return GridView.builder(
-            padding: const EdgeInsets.all(16.0),
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              crossAxisSpacing: 16.0,
-              mainAxisSpacing: 16.0,
-            ),
-            itemCount: hotels.length,
-            itemBuilder: (context, index) {
-              final hotel = hotels[index];
-              return GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => HotelDetailsScreen(hotel: hotel),
-                    ),
-                  );
-                },
-                child: Card(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Expanded(
-                        child: Image.network(
-                          hotel.imageUrl,
-                          fit: BoxFit.cover,
-                        ),
+          return SizedBox(
+            height: 300, // Increased height for hotel cards
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: hotels.length,
+              itemBuilder: (context, index) {
+                final hotel = hotels[index];
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => HotelDetailsScreen(hotel: hotel),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              hotel.name,
-                              style: const TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 16.0,
-                              ),
+                    );
+                  },
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 0.6,
+                    margin: const EdgeInsets.all(8.0),
+                    child: Card(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          Expanded(
+                            child: Image.network(
+                              hotel.imageUrl,
+                              fit: BoxFit.cover,
                             ),
-                            const SizedBox(height: 4.0),
-                            Text(
-                              hotel.location,
-                              style: const TextStyle(
-                                fontSize: 14.0,
-                              ),
-                            ),
-                            const SizedBox(height: 8.0),
-                            Row(
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                const Icon(
-                                  Icons.star,
-                                  color: Colors.amber,
-                                ),
-                                const SizedBox(width: 4.0),
                                 Text(
-                                  hotel.rating.toString(),
+                                  hotel.name,
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 16.0,
+                                  ),
+                                ),
+                                const SizedBox(height: 4.0),
+                                Text(
+                                  hotel.location,
                                   style: const TextStyle(
                                     fontSize: 14.0,
                                   ),
                                 ),
+                                const SizedBox(height: 8.0),
+                                StarRating(rating: hotel.rating),
                               ],
                             ),
-                          ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          );
+        }
+      },
+    );
+  }
+
+Widget buildDestinationList() {
+  return FutureBuilder<List<Destination>>(
+    future: _destinationsFuture,
+    builder: (context, snapshot) {
+      if (snapshot.connectionState == ConnectionState.waiting) {
+        return const Center(child: CircularProgressIndicator());
+      } else if (snapshot.hasError) {
+        return const Center(child: Text('Error loading destinations'));
+      } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+        return const Center(child: Text('No destinations found'));
+      } else {
+        final destinations = snapshot.data!;
+        return SizedBox(
+          height: 200, // Adjust height as needed
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: destinations.length,
+            itemBuilder: (context, index) {
+              final destination = destinations[index];
+              return Container(
+                width: MediaQuery.of(context).size.width * 0.6,
+                margin: const EdgeInsets.all(8.0),
+                child: Card(
+                  child: Stack(
+                    children: [
+                      Positioned.fill(
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(8.0), // Rounded border radius
+                          child: Image.network(
+                            destination.imageUrl,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        bottom: 0,
+                        left: 0,
+                        right: 0,
+                        child: Container(
+                          color: Colors.black54,
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            destination.name,
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
                         ),
                       ),
                     ],
@@ -156,9 +255,51 @@ class _HotelListScreenState extends State<HotelListScreen> {
                 ),
               );
             },
-          );
-        }
-      },
+          ),
+        );
+      }
+    },
+  );
+}
+
+}
+
+class Destination {
+  final String id;
+  final String imageUrl;
+  final String name;
+
+  Destination({required this.id, required this.imageUrl, required this.name});
+
+  factory Destination.fromMap(Map<String, dynamic> data, String id) {
+    return Destination(
+      id: id,
+      imageUrl: data['imageUrl'] as String,
+      name: data['name'] as String,
     );
   }
+}
+
+class StarRating extends StatelessWidget {
+  final double rating;
+
+  const StarRating({Key? key, required this.rating}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: List.generate(5, (index) {
+        return Icon(
+          index < rating ? Icons.star : Icons.star_border,
+          color: Colors.amber,
+        );
+      }),
+    );
+  }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: HotelListScreen(),
+  ));
 }
